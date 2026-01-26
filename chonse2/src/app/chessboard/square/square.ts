71f680485b83afd78418ee3ae02ac8a9285e3ef3
icarus-2 <ebriffett01@gmail.com>
2026-01-26 17:19:45 -0500

    Dark and light colors, rank and file labels work

--- chonse2/src/app/chessboard/square/square.ts
+++ chonse2/src/app/chessboard/square/square.ts
@@ -8,11 +8,27 @@ import { PieceType } from '../piece-type';
   styleUrl: './square.css',
 })
 export class Square {
+
   @Input() coordinate: string = "";
   @Input() piece: string = PieceType.NONE;
+  @Input() isDark: boolean = false;
+  @Input() showRank: boolean = false;
+  @Input() showFile: boolean = true;
+
+  darkColor = "rgb(85,150,242)";
 
   constructor()
   {
 
+  }
+
+  getColor(): string
+  {
+    return this.isDark ? this.darkColor : "white";
+  }
+
+  getReverseColor(): string 
+  {
+    return this.isDark ? "white" : this.darkColor;
   }
 }
