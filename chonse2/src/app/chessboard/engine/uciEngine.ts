a512cfd5f9615daafa318e8b3c02a0d0943d07e8
icarus-2 <ebriffett01@gmail.com>
2026-02-15 20:16:55 -0500

    Added production build

--- chonse2/src/app/chessboard/engine/uciEngine.ts
+++ chonse2/src/app/chessboard/engine/uciEngine.ts
@@ -35,7 +35,7 @@ export class UciEngine {
     }
   }
 
-  static readonly DEFAULT_DEPTH = 14;
+  static readonly DEFAULT_DEPTH = 16;
   public readonly name: EngineName;
   private workers: EngineWorker[] = [];
   private workerQueue: WorkerJob[] = [];
