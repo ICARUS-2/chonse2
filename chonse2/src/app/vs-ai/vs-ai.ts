810455c4ef8bad106a90d74737498ce0051f88b2
icarus-2 <ebriffett01@gmail.com>
2026-02-25 14:50:31 -0500

    Commit before redoing AI config modal

--- chonse2/src/app/vs-ai/vs-ai.ts
+++ chonse2/src/app/vs-ai/vs-ai.ts
@@ -3,6 +3,8 @@ import { ChessBoardService } from '../chessboard/chessboard/chess-board-service'
 import { BoardNames } from '../boards';
 import { Chessboard } from '../chessboard/chessboard/chessboard';
 import BoardState from '../chessboard/chessboard/board-state';
+import Chonse2 from '../../lib/chonse2';
+import { RouteConstants } from '../app.routes';
 
 @Component({
   selector: 'app-vs-ai',
@@ -14,7 +16,8 @@ export class VsAi implements OnInit{
 
   BoardNames = BoardNames;
 
-
+  inputtedPosition: Chonse2 | undefined;
+  
   constructor(public gameService: ChessBoardService)
   {
 
@@ -22,8 +25,24 @@ export class VsAi implements OnInit{
 
   ngOnInit(): void 
   {
+    const routeState = history.state;
+
+    const inputtedPosition = routeState[RouteConstants.ROUTE_INPUTTED_POSITION]
+
+    if (inputtedPosition)
+    {
+
+    }
+    else 
+    {
+      this.setDefault();
+    }
+  }
+
+  setDefault()
+  {
     const bs: BoardState = new BoardState();
     bs.isVsAi = true;
     this.gameService.addGame(BoardNames.VsAi, bs);
-  }
+  } 
 }
