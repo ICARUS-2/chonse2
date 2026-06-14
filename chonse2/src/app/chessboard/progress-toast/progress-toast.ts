c6525520cb3bc69205e220d0eda679219c092d8d
icarus-2 <ebriffett01@gmail.com>
2026-06-13 20:26:50 -0400

    chessboard done

--- chonse2/src/app/chessboard/progress-toast/progress-toast.ts
+++ chonse2/src/app/chessboard/progress-toast/progress-toast.ts
@@ -1,10 +1,11 @@
 import { Component, ElementRef, input, ViewChild, AfterViewInit, ChangeDetectionStrategy } from '@angular/core';
 import { NgbProgressbar } from '@ng-bootstrap/ng-bootstrap';
 import BoardState from '../chessboard/board-state';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-progress-toast',
-  imports: [NgbProgressbar],
+  imports: [NgbProgressbar, TranslatePipe],
   templateUrl: './progress-toast.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrl: './progress-toast.css',
